26b948b1e5ec93719976adb541336ae184567fc6
ShawnBorn-Newton <shawn.newton3@gmail.com>
2017-06-28 19:33:23 -0400

    exercise one

--- ConPractice/Program.cs
+++ ConPractice/Program.cs
@@ -10,6 +10,24 @@ namespace ConPractice
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Shawn's equality tracker.");
+            //enter first number
+            Console.WriteLine("Please enter your first number and press Enter.");
+            double firstNum = int.Parse(Console.ReadLine());
+
+            //enter secon number
+            Console.WriteLine("Please enter your second number and press Enter.");
+            double secondNum = int.Parse(Console.ReadLine());
+            
+            //if equal else not
+            if (firstNum == secondNum)
+            {
+                Console.WriteLine("These numbers are indeed equal.");
+            }
+            else
+            {
+                Console.WriteLine("These number are so not equal.");
+            }
         }
     }
 }
