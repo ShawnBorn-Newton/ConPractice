9fb7427df1a725fe3bcdc79daf048c0497f7390e
ShawnBorn-Newton <shawn.newton3@gmail.com>
2017-06-28 19:56:50 -0400

    exersice 2

--- ConPractice/Program.cs
+++ ConPractice/Program.cs
@@ -10,24 +10,42 @@ namespace ConPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Shawn's equality tracker.");
-            //enter first number
-            Console.WriteLine("Please enter your first number and press Enter.");
-            double firstNum = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Welcome to Shawn's equality tracker.");
+            ////enter first number
+            //Console.WriteLine("Please enter your first number and press Enter.");
+            //double firstNum = double.Parse(Console.ReadLine());
 
-            //enter secon number
-            Console.WriteLine("Please enter your second number and press Enter.");
-            double secondNum = int.Parse(Console.ReadLine());
+            ////enter secon number
+            //Console.WriteLine("Please enter your second number and press Enter.");
+            //double secondNum = double.Parse(Console.ReadLine());
             
-            //if equal else not
-            if (firstNum == secondNum)
-            {
-                Console.WriteLine("These numbers are indeed equal.");
-            }
-            else
-            {
-                Console.WriteLine("These number are so not equal.");
-            }
+            ////if equal else not
+            //if (firstNum == secondNum)
+            //{
+            //    Console.WriteLine("These numbers are indeed equal.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("These number are so not equal.");
+            //}
+
+            ////exerise 2
+            //Console.WriteLine("Is your number even or odd? Let's find out!");
+            ////enter number
+
+            //Console.WriteLine("Please enter your number and press Enter.");
+            //double num = double.Parse(Console.ReadLine());
+
+            ////if and else
+            //if (num % 2 == 0)
+            //{
+            //    Console.WriteLine("This numer is even.");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("This numer is odd.");
+            //}
         }
     }
 }
